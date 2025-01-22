d69bff5e5caee4811d386c72b4bfbb78a5c7565a
enoreyes <enoreyes@gmail.com>
2025-01-22 11:01:40 -0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,78 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {parser} from "./terraform.grammar"
+import {
+  LRLanguage,
+  LanguageSupport,
+  foldNodeProp,
+  foldInside,
+  indentNodeProp,
+  delimitedIndent
+} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+/**
+ * Create a language definition using the Terraform grammar.
+ */
+export const TerraformLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
+      styleTags({
+        Boolean: t.bool,
+        FloatExp: t.number,
+        FloatNoExp: t.number,
+        Int: t.number,
+        StringToken: t.string,
+        Identifier: t.variableName,
+
+        // Operators, punctuation
+        EqualEqual: t.operatorKeyword,
+        NotEqual: t.operatorKeyword,
+        GreaterEq: t.operatorKeyword,
+        LessEq: t.operatorKeyword,
+        AndAnd: t.operatorKeyword,
+        OrOr: t.operatorKeyword,
+        Arrow: t.operatorKeyword,
+        Greater: t.operatorKeyword,
+        Less: t.operatorKeyword,
+        Plus: t.operatorKeyword,
+        Minus: t.operatorKeyword,
+        Star: t.operatorKeyword,
+        Slash: t.operatorKeyword,
+        Mod: t.operatorKeyword,
+        Bang: t.operatorKeyword,
+        Eq: t.operatorKeyword,
+
+        LBrace: t.brace,
+        RBrace: t.brace,
+        LBracket: t.squareBracket,
+        RBracket: t.squareBracket,
+        LParen: t.paren,
+        RParen: t.paren,
+
+        LineComment: t.lineComment,
+        BlockComment: t.blockComment
+      }),
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Block: delimitedIndent({closing: "}", align: false}),
+        ObjectLit: delimitedIndent({closing: "}", align: false}),
+        ArrayLit: delimitedIndent({closing: "]", align: false}),
+        Parens: delimitedIndent({closing: ")", align: false})
       }),
       foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
+        Block: foldInside,
+        ObjectLit: foldInside,
+        ArrayLit: foldInside
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: {line: "//"}
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+/**
+ * Export a helper function that returns
+ * a LanguageSupport instance for Terraform.
+ */
+export function terraform() {
+  return new LanguageSupport(TerraformLanguage)
 }
